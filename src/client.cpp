18bddde524b8de062e4a8c33eceec505e0312f36
Huibing Dong <huibingdong@Huibings-MacBook-Pro.local>
2020-02-17 23:14:37 -0600

    Change Client Interface: add subscribe lists

--- src/client.cpp
+++ src/client.cpp
@@ -4,4 +4,8 @@
 Client::Client(char *ip_, int port_){
     this->port = port_;
     inet_pton(AF_INET,ip_,&this->ip);
+
+    sub_types.clear();
+    sub_originators.clear();
+    sub_orgs.clear();
 }
--- src/client.h
+++ src/client.h
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class Client{
     public:
     int ip;
     int port;
 
+    vector<string> sub_types;
+    vector<string> sub_originators;
+    vector<string> sub_orgs;
     Client(char *ip, int port);
 };
